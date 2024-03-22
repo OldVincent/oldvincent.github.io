40f1f2f3d08bca2dd51f8c2f6a2f68eff07c3074
Haoyu Jia <57647667+OldVincent@users.noreply.github.com>
2024-03-22 23:49:49 +0800

    Upload author avatar photo.

--- src/.vuepress/author.ts
+++ src/.vuepress/author.ts
@@ -5,6 +5,8 @@ const options: BlogLocaleOptions = {
     "Software development addict. Founder of open-source organization <i>Pygmalions</i>. <br/> \
     Currently dedicated to Robot Software Engineering and related software infrastures.",
   intro: "/resume.html",
+  avatar: "/author_avatar.png",
+  roundAvatar: true,
   medias: {
     Email: "mailto:jia_vincent@outlook.com",
     GitHub: "https://github.com/OldVincent",
